482834c802bc08a628e27f32c530eeb36d227940
MD-KAIF16 <mdkaif.1608@gmail.com>
2026-09-11 09:09:09 +0530

    Time: 36 ms (28.38%), Space: 59.3 MB (45.11%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,55 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
         List<List<Integer>> res = new ArrayList<>();
 
+        int n = nums.length ;
         Arrays.sort(nums);
+        
+        for(int i = 0 ; i < n-2 ; i++){
 
-        int n = nums.length;
+            //skip ith repeatition
+            if(i > 0 && nums[i]==nums[i-1]) continue;
 
-        for (int i = 0; i < n - 2; i++) {
+            int l = i+1;
+            int r = n-1;
+        while(l < r){
+            int sum = nums[i] + nums[l] + nums[r];
 
-            // Skip duplicate first elements
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
+            if(sum == 0){
+                res.add(Arrays.asList(nums[i], nums[l] , nums[r]));
+
+                while(l<r && nums[r]==nums[r-1])  r--;
+
+                while(l<r && nums[l]==nums[l+1])  l++;
+
+                l++;
+                r--;
             }
 
-            int l = i + 1;
-            int r = n - 1;
+            else if(sum > 0)   r--;
+            else l++;
 
-            while (l < r) {
-
-                int sum = nums[i] + nums[l] + nums[r];
-
-                if (sum == 0) {
-
-                    res.add(Arrays.asList(
-                        nums[i],
-                        nums[l],
-                        nums[r]
-                    ));
-
-                    // Skip duplicates
-                    while (l < r && nums[l] == nums[l + 1]) {
-                        l++;
-                    }
-
-                    while (l < r && nums[r] == nums[r - 1]) {
-                        r--;
-                    }
-
-                    // Move both pointers
-                    l++;
-                    r--;
-                }
-
-                else if (sum > 0) {
-                    r--;
-                }
-
-                else {
-                    l++;
-                }
             }
         }
 
