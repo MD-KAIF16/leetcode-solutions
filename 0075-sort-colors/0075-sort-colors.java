9709118f401f6749759aba5a1b842821d6d81fa6
MD-KAIF16 <mdkaif.1608@gmail.com>
2026-09-11 09:30:12 +0530

    Time: 0 ms (100%), Space: 43.4 MB (76.08%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,30 +1,32 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int low = 0;
+        int low = 0 ;
         int mid = 0;
-        int high = nums.length - 1;
+        int high = nums.length-1 ;
+
 
         while(mid <= high){
-            if(nums[mid]==0){
-               nums[mid]=nums[low];
-               nums[low] = 0;
-               low++;
-               mid++;
+
+            if(nums[mid] == 0){
+                nums[mid] = nums[low];
+                nums[low] = 0;
+
+                mid++;
+                low++;
+
 
             }
+
             else if(nums[mid] == 1){
-                  mid++;
+                mid++;
 
             }
-
             else{
-                
-                 nums[mid] = nums[high];
-                 nums[high] = 2;
-
-                 high--;
-
+                nums[mid] = nums[high];
+                nums[high] = 2;
+                high --;
             }
+
         }
     }
 }
