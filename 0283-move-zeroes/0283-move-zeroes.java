a74e6395a5097485977cf3038a150237ee376cb6
MD-KAIF16 <mdkaif.1608@gmail.com>
2026-09-11 09:39:59 +0530

    Time: 2 ms (91.76%), Space: 48.1 MB (6.92%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,18 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        int n = nums.length ;
+
         int left = 0;
-        for(int right = 0; right < nums.length ; right++){
-            if(nums[right] != 0){
+        
+
+        for(int right = 0 ; right < n ; right++){
+             if(nums[right]!=0){
                 int temp = nums[right];
-                nums[right]  = nums[left];
-                nums[left] = temp ;
+                nums[right] = nums[left];
+                nums[left] = temp;
 
                 left++;
-            }
+             }
         }
     }
 }
