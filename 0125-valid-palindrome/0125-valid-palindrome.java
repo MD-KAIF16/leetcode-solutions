a39a67143dd29ca5a0ffc09bbfd9c10014e85e23
MD-KAIF16 <mdkaif.1608@gmail.com>
2026-09-19 15:05:22 +0530

    Time: 2 ms (99.31%), Space: 44.1 MB (91.02%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,52 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        //In Question
+        int n = s.length();
+        int i = 0;
+        int j = n-1 ;
 
-        //1. Avoid Upper and lower -> I Choose Letter in LowerCase  (.isLowerCase())
-        //2. Avoid Non Alpha numeric -> I avoid (.isLetterOrDigit())
+        while(i < j){
+            char left = s.charAt(i);
+            char right = s.charAt(j);
 
-        //Start taking both the ends pointer 
-
-        int i = 0 ;
-        int j = s.length() - 1;
-
-        //Use Two Pointer Approach from both the end 
-        while(i<j){
-            //taking both the characters first 
-            char left = s.charAt(i);  //Left Char
-            char right = s.charAt(j);  //Right char 
-
-            //Finding exact matching position for bpth the pointers by eliminating non ulpha numeric character
-
-            //For i pointer 
-            if(!Character.isLetterOrDigit(left)){  //if character is alphanumeric condition become true 
-                 i = i+1; //so i increment the pointer 
-                 continue ;  //current iteration ko skip krdega 
+            if(!Character.isLetterOrDigit(left)){
+                i++;
+                continue ;
+            }
+            if(!Character.isLetterOrDigit(right)){
+                j--;
+                continue ;
             }
 
-            //For j pointer 
-            if(!Character.isLetterOrDigit(right)){  //if character is alphanumeric condition become true 
-                 j=j-1; //so j decrement the pointer 
-                 continue ; //current iteration ko skip krdega 
+            if(Character.toLowerCase(left) != Character.toLowerCase(right)){
+                return false;
             }
 
-            //Last Main step comparing both the character in lowercase -> toLowerCase()
-            if(Character.toLowerCase(left) != Character.toLowerCase(right))  
-             ////Comparing both the left and right characters if condition becomes true return false 
-            {
-               return false ;
-            }
-
-            //if while condition not completeed 
-            i=i+1;
-            j=j-1;
-
+            i++;
+            j--;
         }
- 
 
-        //if nothing is happens then 
-        return true ;
-
-        
+        return true;
     }
 }
