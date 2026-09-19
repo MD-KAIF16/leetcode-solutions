d6d089998aadc5ae1770a8ad127ae31f18e88574
MD-KAIF16 <mdkaif.1608@gmail.com>
2026-09-19 14:50:00 +0530

    Time: 0 ms (100%), Space: 48.2 MB (73.82%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,23 +1,16 @@
 class Solution {
     public void reverseString(char[] s) {
-        //In Question
-
-        //Swap the given character string without using extra space [O(1)] and in place 
-
-        //by Two pointer approach
+        int n = s.length;
         int i = 0 ;
-        int j = s.length - 1 ;
+        int j = n-1;
 
-        while(i < j){
+        while(i<j){
             char temp = s[i];
-            s[i] = s[j];         //Cross logc method
+            s[i] = s[j];
             s[j] = temp;
-        
-         //after operation increment left ponter and decrement right pointer
+
             i++;
             j--;
         }
-
-        //no return because of void return type
     }
 }
